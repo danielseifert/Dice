6249af672f16ba1d0eff9f42ff101b0105240c0b
Daniel Seifert <das.mcs1998@gmail.com>
2017-03-30 14:44:24 -0700

    Remove unnecessary Dice modules and create single, catch-all function. Users can now specify n for n-sided dice

--- dice.exs
+++ dice.exs
@@ -1,105 +1,18 @@
 defmodule Dice do
 
-  defmodule D4 do
-    def d4(num) do
-      roll_d4(num, 0)
-    end
-
-    defp roll_d4(0, acc) do
-      acc
-    end
-
-    defp roll_d4(num, acc) do
-      result = Enum.random(1..4)
-      acc = acc + result
-      num = num - 1
-      roll_d4(num, acc)
-    end
+  def roll(num_d, num_s) do
+    roll_d(num_d, 0, num_s)
   end
 
-  defmodule D6 do
-    def d6(num) do
-      roll_d6(num, 0)
-    end
-
-    defp roll_d6(0, acc) do
-      acc
-    end
-
-    defp roll_d6(num, acc) do
-      result = Enum.random(1..6)
-      acc = acc + result
-      num = num - 1
-      roll_d6(num, acc)
-    end
+  def roll_d(0, acc, _num_s) do
+    acc
   end
 
-  defmodule D8 do
-    def d8(num) do
-      roll_d8(num, 0)
-    end
-
-    defp roll_d8(0, acc) do
-      acc
-    end
-
-    defp roll_d8(num, acc) do
-      result = Enum.random(1..8)
-      acc = acc + result
-      num = num - 1
-      roll_d8(num, acc)
-    end
-  end
-
-  defmodule D10 do
-    def d10(num) do
-      roll_d10(num, 0)
-    end
-
-    defp roll_d10(0, acc) do
-      acc
-    end
-
-    defp roll_d10(num, acc) do
-      result = Enum.random(1..10)
-      acc = acc + result
-      num = num - 1
-      roll_d10(num, acc)
-    end
-  end
-
-  defmodule D12 do
-    def d12(num) do
-      roll_d12(num, 0)
-    end
-
-    defp roll_d12(0, acc) do
-      acc
-    end
-
-    defp roll_d12(num, acc) do
-      result = Enum.random(1..12)
-      acc = acc + result
-      num = num - 1
-      roll_d12(num, acc)
-    end
-  end
-
-  defmodule D20 do
-    def d20(num) do
-      roll_d20(num, 0)
-    end
-
-    defp roll_d20(0, acc) do
-      acc
-    end
-
-    defp roll_d20(num, acc) do
-      result = Enum.random(1..20)
-      acc = acc + result
-      num = num - 1
-      roll_d20(num, acc)
-    end
+  def roll_d(num_d, acc, num_s) do
+    result = Enum.random(1..num_s)
+    acc = acc + result
+    num_d = num_d - 1
+    roll_d(num_d, acc, num_s)
   end
 
 end
